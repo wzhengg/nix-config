b6aec7672abf5c6fda7080ec60fdaac76ae12806
William Zheng <williamzheng2001@gmail.com>
2024-09-17 21:44:28 -0400

    use latest linux kernel

--- configuration.nix
+++ configuration.nix
@@ -3,6 +3,8 @@
 {
   imports = [ ./hardware-configuration.nix ];
 
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
